aff0940a67848b04265c25c22956e1d12e2e56f0
andersonlc2 <acorreia610@gmail.com>
2022-01-26 23:24:38 -0300

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,70 @@
+package application;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+import org.openjsse.util.RSAKeyUtil;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        System.out.println("Enter client data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("E-mail: ");
+        String email = sc.nextLine();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println();
+        System.out.println("Enter order data: ");
+        System.out.print("Status: ");
+        OrderStatus status = OrderStatus.valueOf(sc.next());
+        Order order = new Order(
+                new Date(),
+                status,
+                client
+        );
+        System.out.print("How many items to this order? ");
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) {
+            System.out.printf("Enter #%d item data:\n", i);
+            System.out.print("Product name: ");
+            sc.nextLine();
+            String productName = sc.nextLine();
+            System.out.print("Produtc price: ");
+            Double productPrice = sc.nextDouble();
+            System.out.print("Quantity: ");
+            Integer quantity = sc.nextInt();
+            Product product = new Product(productName, productPrice);
+            OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+            order.addItem(orderItem);
+        }
+
+        System.out.println();
+        System.out.println("ORDER SUMARY:");
+        System.out.println("Order moment: "+sdf1.format(order.getMoment()));
+        System.out.println("Order status: "+order.getStatus());
+        System.out.println("Client: "+order.getClient());
+        System.out.println("Order Items: ");
+        for (OrderItem item : order.getItems()) {
+            System.out.println(item);
+        }
+        System.out.printf("Total price: %.2f", order.total());
+    }
+}
